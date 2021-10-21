ca9780386f78129d0eb09968a964b197b263d139
Ives Ebneter <ives.ebneter@gmail.com>
2021-10-21 17:11:48 +0200

    basic setup

---
+++ components/MainNav.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue'
+
+const navLinks = ref([
+  {
+    label: 'Home',
+    url: '/'
+  },
+  {
+    label: 'Todo',
+    url: '/todo'
+  }
+])
+
+const useColor = ref('')
+</script>
+
+<template>
+  <nav class="main-nav">
+    <ul>
+      <li v-for="item in navLinks">
+        <nuxt-link :to="item.url">{{item.label}}</nuxt-link>
+      </li>
+      <li><input type="color" v-model="useColor"></li>
+    </ul>
+  </nav>
+</template>
+
+<style>
+.main-nav ul {
+  display: flex;
+}
+.main-nav li {
+  list-style: none;
+  padding: 0 12px;
+}
+
+.main-nav .router-link-active {
+  background-color: v-bind(useColor);
+}
+</style>
