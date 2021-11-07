78e0915330463acca2d229a553b828ceed55216b
Ives Ebneter <ives.ebneter@gmail.com>
2021-11-07 12:59:08 +0100

    add windicss

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,24 @@
+<!-- TODO refactor to use script setup -->
+<script >
+export default {
+  setup () {
+    useMeta({
+      title: '🐶 Ivester\'s Profile Page',
+      bodyAttrs: {
+        class: 'bg-grey-100 font-sans leading-normal tracking-normal'
+      }
+    })
+  }
+}
+</script>
+
 <template>
   <div>
-    <MainNav />
-    <slot name="header" class="bg">Default header</slot>
+    <!-- <MainNav /> -->
+    <!-- <slot name="header" class="bg">Default header</slot> -->
     <slot name="default"/>
-    <slot name="footer">
+    <!-- <slot name="footer">
       <footer>Copyright {{new Date().getFullYear()}}</footer>
-    </slot>
+    </slot> -->
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,13 @@ export default {}
 
 <template>
   <div>
-    <h1>Home Page</h1>
+    <div class="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-red-900 to-purple-700">
+      <div class="text-center text-5xl font-extrabold">
+        <!-- TODO fix cut off text -->
+        <h1 class="bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">Home Page</h1>
+      </div>
+    </div>
+    <div>Section 2</div>
   </div>
 </template>
 
